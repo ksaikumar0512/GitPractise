cc3518e3c33cb8f89fb22f2debcf44a2d77f8f9c
ksaikumar0512 <72634225+ksaikumar0512@users.noreply.github.com>
2023-04-25 14:57:15 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,45 @@
+package java2Json;
+//import com.google.gson.Gson;
+
+class Student {
+    private int id;
+    private String name;
+
+
+    private int age;
+    private long phone;
+    private long mobilenumber;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public long getPhone() {
+        return phone;
+    }
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+
+    public long getMobilenumber() {
+        return mobilenumber;
+    }
+
+    public void setMobilenumber(long mobilenumber) {
+        this.mobilenumber = mobilenumber;
+    }
+}
